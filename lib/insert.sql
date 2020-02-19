db188c9134acae4083d8c438d24b91ed41333f48
Anthony Pardo <pardo.anthony@gmail.com>
2020-02-19 20:27:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,3 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 15, "Male", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 11, "Male", "Brown", "Rowdy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("", 15, "Male", "Brown", "Angry", 1);
